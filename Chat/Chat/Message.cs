d3b070c76393df6556b548ac9fe53d0835a0c476
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-11-22 22:09:53 +0500

    Домашнее задание по семинару1

---
+++ Chat/Chat/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Chat
+{//1. Мы пишем простое чат-приложение способное передавать сообщения с компьютера на компьютер.
+ // Начнем с разработки модели сообщений для нашего чата.
+ // Договоримся что у каждого пользователя может быть свой ник-нейм - уникальное имя.
+ // Ему можно будет передать сообщение, состоящее из даты, никнейма отправителя  и текста сообщения.
+ // Как мог бы выглядеть класс представляющий сообщение в этом случае.
+
+    //2.Добавим JSON сериализацию и десериализацию в наш класс.
+    //И протестируем его путем компоновки простого сообщения, сериализации и десериализации этого сообщения.
+    public class Message
+    {
+        public string NickName {  get; set; }
+        public DateTime DateMessage { get; set; }
+        public string TextMessage {  get; set; }
+
+        public string MessageToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static Message MessageFromJson(string json)
+        {
+            return JsonSerializer.Deserialize<Message>(json) ?? new Message();
+        }
+
+    }
+
+    
+}
---
+++ Chat/Chat/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat
+{
+
+    //Попробуйте переработать приложение,
+    //добавив подтверждение об отправке сообщений как в сервер, так и в клиент.
+
+
+    internal class Program
+    {
+        public static void Server()
+        {
+
+            UdpClient udpServer = new UdpClient(12345);
+           
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+           
+            Console.WriteLine("Ожидаем сообщение от пользователя");
+           
+           
+
+            while(true)
+            {
+                try
+                {
+                    byte[] buffer = udpServer.Receive(ref iPEndPoint);
+
+                    string data = Encoding.ASCII.GetString(buffer);
+
+                    var dataMessage = Message.MessageFromJson(data);
+
+                    Console.WriteLine($" Получено сообщение от {dataMessage.NickName} + время сообщения {dataMessage.DateMessage} ");
+
+                    Console.WriteLine($"{dataMessage.TextMessage}");
+                    
+                    string answer = "Сообщение получено";
+
+                    var mess = new Message()
+                    {
+                        NickName = dataMessage.NickName,
+                        DateMessage = DateTime.Now,
+                        TextMessage = answer
+
+
+                    };
+
+                    data = mess.MessageToJson();
+
+                    buffer = Encoding.ASCII.GetBytes(data);
+
+                    udpServer.Send(buffer, buffer.Length, iPEndPoint);
+
+
+
+                } catch ( Exception ex ) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+        }
+
+        public static void Client(string name, string ip, string message)
+        {
+
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+           
+            Message mess = new Message()
+            { NickName = name,
+                DateMessage = DateTime.Now, 
+                TextMessage = message};
+
+            try
+            {
+                var data = mess.MessageToJson();
+
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+
+                udpClient.Send(buffer, buffer.Length, iPEndPoint);
+
+                Console.WriteLine("Сообщение отправлено");
+
+                buffer = udpClient.Receive(ref iPEndPoint);
+
+                data = Encoding.ASCII.GetString(buffer);
+
+                var dataMessage = Message.MessageFromJson(data);
+
+                Console.WriteLine($" Получено сообщение от {dataMessage.NickName} + время сообщения {dataMessage.DateMessage} ");
+
+                Console.WriteLine($"{dataMessage.TextMessage}");
+
+
+            } catch( Exception ex )
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+           
+            if (args.Length == 0) {
+                Server();
+            }
+            else
+            {
+                
+                    new Thread(() => Client(args[0], args[1], args[2]))
+                    .Start();
+
+            }
+
+        }
+    }
+}
