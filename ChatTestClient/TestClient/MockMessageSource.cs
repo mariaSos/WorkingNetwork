406a5630ebf99343e71570886ce0b30d7e18c1bf
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-12-11 23:06:58 +0500

    Домашнее задание по семинару 6

---
+++ ChatTestClient/TestClient/MockMessageSource.cs
@@ -0,0 +1,70 @@
+﻿using ChatTestClient.Abstraction;
+using ChatTestClient.Models;
+using ChatTestClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    public class MockMessageSource : IMessageSource
+    {
+
+        private Client client;
+
+        private IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        private Queue<MessageUdp> messages = new Queue<MessageUdp>();
+
+
+
+        public MockMessageSource()
+        {
+            messages.Enqueue(new MessageUdp { Command = Command.Register, FromName = "Вася" });
+            messages.Enqueue(new MessageUdp { Command = Command.Register, FromName = "Юля" });
+            messages.Enqueue(new MessageUdp { Command = Command.Message, FromName = "Юля", ToName = "Вася", Text = "От Юли" });
+            messages.Enqueue(new MessageUdp { Command = Command.Message, FromName = "Вася", ToName = "Юля", Text = "От Васи" });
+        }
+
+
+      public MessageUdp Receive(ref IPEndPoint ep)
+        {
+              ep = endPoint;
+
+            if (messages.Count == 0)
+            {
+                client.Stop();
+                return null;
+            }
+            return messages.Dequeue();
+        }
+
+
+        public void Send(MessageUdp message, IPEndPoint ep)
+        {
+             ep = endPoint;
+
+
+            if (messages.Count == 0)
+            {
+                client.Stop();
+
+
+            }
+            messages.Dequeue();
+    }
+
+
+
+        public void AddClient(Client client)
+        {
+            this.client = client;
+
+        }
+
+
+    }
+}
---
+++ ChatTestClient/TestClient/Test.cs
@@ -0,0 +1,81 @@
+using ChatTestClient.Models;
+using ChatTestClient.Services;
+
+namespace TestClient
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            using (MessageContext ctx = new MessageContext())
+            {
+                ctx.Messages.RemoveRange(ctx.Messages);
+                ctx.Users.RemoveRange(ctx.Users);
+
+                ctx.SaveChanges();
+            }
+
+
+        }
+
+         [TearDown]
+         public void TearDown()
+         {
+             using (MessageContext ctx = new MessageContext())
+             {
+                 ctx.Messages.RemoveRange(ctx.Messages);
+                 ctx.Users.RemoveRange(ctx.Users);
+
+                 ctx.SaveChanges();
+             }
+
+
+         }
+
+
+        [Test]
+        public void ClientTest()
+        {
+
+            var mock = new MockMessageSource();
+
+            var client1 = new Client(mock, "127.0.0.1", 5522, "Юля");
+            var client2 = new Client(mock, "127.0.0.1", 5533, "Вася");
+
+            mock.AddClient(client1);
+            mock.AddClient(client2);
+          
+                client1.Start();
+                client2.Start();
+               
+
+          
+                using (MessageContext ctx = new MessageContext())
+                {
+                     Assert.IsTrue(ctx.Users.Count() == 2, "Пользователи не созданы");
+
+                    var user1 = ctx.Users.FirstOrDefault(x => x.Name == "Вася");
+                    var user2 = ctx.Users.FirstOrDefault(x => x.Name == "Юля");
+
+                    Assert.IsNotNull(user1, "Пользователь не создан");
+                    Assert.IsNotNull(user2, "Пользователь не создан");
+
+                    Assert.IsTrue(user1.FromMessages.Count == 1);
+                    Assert.IsTrue(user2.FromMessages.Count == 1);
+
+                    Assert.IsTrue(user1.ToMessage.Count == 1);
+                    Assert.IsTrue(user2.ToMessage.Count == 1);
+
+                    var msg1 = ctx.Messages.FirstOrDefault(x => x.FromUser == user1 && x.ToUser == user2);
+                    var msg2 = ctx.Messages.FirstOrDefault(x => x.FromUser == user2 && x.ToUser == user1);
+
+                    Assert.AreEqual("От Юли", msg2.Text);
+                    Assert.AreEqual("От Васи", msg1.Text);
+
+
+                }
+
+        }
+    }
+}
