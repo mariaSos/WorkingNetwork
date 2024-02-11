a9c6440a61391f6e9f73fd8e1bcb371c5779685e
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2024-02-12 00:42:13 +0500

    ASP.Net. Домашнее задание 2

---
+++ StoreMarket2/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreMarket.Abstractions;
+using StoreMarket.Contexts;
+using StoreMarket.Contracts.Requests;
+using StoreMarket.Contracts.Responses;
+using StoreMarket.Models;
+
+namespace StoreMarket.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+
+    public class ProductController : ControllerBase
+    {
+
+        private readonly IProductServices _productServices;
+
+        public ProductController(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+
+        [HttpGet]
+        [Route("products/{id}")]
+
+        public ActionResult<ProductResponse> GetProductById(int id)
+        {
+
+            var product = _productServices.GetProductById(id);
+
+            return Ok(product);
+
+        }
+
+
+        [HttpGet]
+        [Route("products")]
+
+        public ActionResult<IEnumerable<ProductResponse>> GetProducts()
+        {
+            var products = _productServices.GetProducts();
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [Route("addProduct")]
+
+        public ActionResult<ProductResponse> AddProducts(ProductCreateRequest request)
+        {
+            
+            try {
+                var id = _productServices.AddProduct(request);
+                return Ok(id);
+
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+
+        [HttpDelete]
+        [Route("productsDelete/{id}")]
+
+        public ActionResult<ProductResponse> RemoveProduct(ProductDeleteRequest request) { 
+        try
+        {
+            var id = _productServices.RemoveProduct(request);
+            return Ok(id);
+        }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        
+    }
+}
